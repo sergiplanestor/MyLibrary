2b7a17d1143c196280aca77cbd17b22df59dd224
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-10 17:25:56 +0100

    Update BookService.kt

--- app/src/main/java/bemobile/splanes/com/mylibrary/framework/book/BookService.kt
+++ app/src/main/java/bemobile/splanes/com/mylibrary/framework/book/BookService.kt
@@ -1,15 +1,31 @@
 package bemobile.splanes.com.mylibrary.framework.book
 
-class BookService() : BookDataSource {
-
+class BookService(private val restApiRepository: RestApiRepository) : BookDataSource {
+    
+    companion object {
+        EXPIRATION_DATA = 3600000 // 1h
+    }
+    
+    private val bookCache: MutableList<Book> = listOf<Book>().toMutableList()
+    private var nextUpdate: Long = 0L
+    
     override suspend fun add(book: Book) {
     }
+    
     override suspend fun fetchAll(): List<Book> {
+        if (RestUtils.isDataExpirated<Book>(bookCache, out nextUpdate)) {
+            // TODO: Rest call
+        } else {
+            // TODO: Cache 
+        }
     }
+    
     override suspend fun fetch(id: Int): Book {
     }
+    
     override suspend fun remove(book: Book) {
     }
+    
     override suspend fun update(book: Book) {
     }
 }
