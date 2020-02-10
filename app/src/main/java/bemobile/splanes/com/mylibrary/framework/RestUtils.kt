867b32bf38d4330988625219f832bc39d2640f28
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-10 18:03:02 +0100

    Update RestUtils.kt

--- app/src/main/java/bemobile/splanes/com/mylibrary/framework/RestUtils.kt
+++ app/src/main/java/bemobile/splanes/com/mylibrary/framework/RestUtils.kt
@@ -1,6 +1,21 @@
+package bemobile.splanes.com.mylibrary.framework
+
 
 object RestUtils {
 
-
-
+    fun <T> isDataExpirated(items: List<T>, nextUpdate: Long) : Boolean {
+        
+        if (items.isEmpty) {
+            return true
+        }
+        
+        val currentMillis = System.currentTimeMillis()
+        
+        if (nextUpdate < currentMillis) {
+            nextUpdate = currentMillis + BookService.EXPIRATION_DATA
+            return true
+        } else {
+            return false
+        }        
+    }
 }
