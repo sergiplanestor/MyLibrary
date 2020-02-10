ee56fac8e617d112e04a15bedbd99107d15a7036
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-10 16:29:13 +0100

    Update App.kt

--- app/src/main/java/bemobile/splanes/com/mylibrary/app/App.kt
+++ app/src/main/java/bemobile/splanes/com/mylibrary/app/App.kt
@@ -1,13 +1,18 @@
 package bemobile.splanes.com.mylibrary.app
 
 import android.app.Application
-import bemobile.splanes.com.mylibrary.app.koins.appModule
+import bemobile.splanes.com.mylibrary.app.koins.repositoryModule
+import bemobile.splanes.com.mylibrary.app.koins.dataSourceModule
+import bemobile.splanes.com.mylibrary.app.koins.useCaseModule
 import bemobile.splanes.com.mylibrary.app.koins.networkModule
 import org.koin.android.ext.android.startKoin
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin(androidContext = this, modules = listOf(appModule, networkModule))
+        startKoin(androidContext = this, modules = listOf(repositoryModule,
+                                                          dataSourceModule,
+                                                          useCaseModule,
+                                                          networkModule))
     }
 }
