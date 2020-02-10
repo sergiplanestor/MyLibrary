6fc6f8a3cd2183c4e4dbb743a5b225a0fa2c84ff
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-10 16:48:49 +0100

    Update BookService.kt

--- app/src/main/java/bemobile/splanes/com/mylibrary/framework/book/BookService.kt
+++ app/src/main/java/bemobile/splanes/com/mylibrary/framework/book/BookService.kt
@@ -1,8 +1,15 @@
 package bemobile.splanes.com.mylibrary.framework.book
 
-class BookService() {
+class BookService() : BookDataSource {
 
-  
-
-
+    override suspend fun add(book: Book) {
+    }
+    override suspend fun fetchAll(): List<Book> {
+    }
+    override suspend fun fetch(id: Int): Book {
+    }
+    override suspend fun remove(book: Book) {
+    }
+    override suspend fun update(book: Book) {
+    }
 }
