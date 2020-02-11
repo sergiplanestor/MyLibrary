b62867edf36b855c2307a4b67fce7c99921aa2fa
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:15:13 +0100

    Update RemoveBookUseCase.kt

--- core/src/main/java/bemobile/splanes/com/core/interactor/book/RemoveBookUseCase.kt
+++ core/src/main/java/bemobile/splanes/com/core/interactor/book/RemoveBookUseCase.kt
@@ -4,6 +4,11 @@ import bemobile.splanes.com.core.data.repository.BookRepository
 import bemobile.splanes.com.core.domain.Book
 
 class RemoveBookUseCase(private val bookRepository: BookRepository) {
-    suspend operator fun invoke(book: Book) = bookRepository.removeBook(book)
-
+    suspend operator fun invoke(
+        
+        book: Book,
+        onSuccess: (isBookRemoved: Boolean) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+    
+    ) = bookRepository.removeBook(book = book, onSuccess = onSuccess, onError = onError)
 }
