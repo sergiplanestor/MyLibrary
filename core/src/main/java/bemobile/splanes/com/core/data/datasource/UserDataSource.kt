5ad0a313750c9c75da0ccc06ee300e2be80e9629
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:06:07 +0100

    Update UserDataSource.kt

--- core/src/main/java/bemobile/splanes/com/core/data/datasource/UserDataSource.kt
+++ core/src/main/java/bemobile/splanes/com/core/data/datasource/UserDataSource.kt
@@ -1,10 +1,17 @@
-package bemobile.splanes.com.core.data.datasource
+package bemobile.splanes.com.core.data.repository
 
+import bemobile.splanes.com.core.data.datasource.UserDataSource
 import bemobile.splanes.com.core.domain.User
 
-interface UserDataSource {
+class UserRepository(private val userDataSource: UserDataSource) {
 
-    fun getStoredUser() : User?
+    fun getStoredUser() : User? = userDataSource.getStoredUser()
 
-    suspend fun registerUser(user: User) : Boolean
+    suspend fun registerUser(
+        
+        user: User,
+        onSuccess: (isUserRegistered: Boolean) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+    
+    ) = userDataSource.registerUser(user = user, onRequestSuccess = onSuccess, onRequestError = onError)
 }
