d4a7fd9002b7f3815f40768c7faf9ea0b3033185
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:17:33 +0100

    Update UpdateBookUseCase.kt

--- core/src/main/java/bemobile/splanes/com/core/interactor/book/UpdateBookUseCase.kt
+++ core/src/main/java/bemobile/splanes/com/core/interactor/book/UpdateBookUseCase.kt
@@ -4,5 +4,11 @@ import bemobile.splanes.com.core.data.repository.BookRepository
 import bemobile.splanes.com.core.domain.Book
 
 class UpdateBookUseCase(private val bookRepository: BookRepository) {
-    suspend operator fun invoke(book: Book) = bookRepository.updateBook(book)
+    suspend operator fun invoke(
+        
+        book: Book,
+        onSuccess: (isBookUpdated: Boolean) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+    
+    ) = bookRepository.updateBook(book = book, onSuccess = onSuccess, onError = onError)
 }
