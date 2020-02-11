df12399170cb9ef63ea90b0027f0d942043c2cfb
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:20:16 +0100

    Update AddTagUseCase.kt

--- core/src/main/java/bemobile/splanes/com/core/interactor/tag/AddTagUseCase.kt
+++ core/src/main/java/bemobile/splanes/com/core/interactor/tag/AddTagUseCase.kt
@@ -5,5 +5,12 @@ import bemobile.splanes.com.core.domain.Book
 import bemobile.splanes.com.core.domain.Tag
 
 class AddTagUseCase(private val tagRepository: TagRepository) {
-    suspend operator fun invoke(tags: List<Tag>, book: Book) = tagRepository.addTag(tags, book)
+    suspend operator fun invoke(
+        
+        tags: List<Tag>,
+        book: Book,
+        onSuccess: (isTagAdded: Boolean) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+    
+    ) = tagRepository.addTag(tags = tags, book = book, onSuccess = onSuccess, onError = onError)
 }
