f6b2e6c7203a379a89a8673cd3fbae34218466fc
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:13:04 +0100

    Update GetBookUseCase.kt

--- core/src/main/java/bemobile/splanes/com/core/interactor/book/GetBookUseCase.kt
+++ core/src/main/java/bemobile/splanes/com/core/interactor/book/GetBookUseCase.kt
@@ -4,5 +4,11 @@ import bemobile.splanes.com.core.data.repository.BookRepository
 import bemobile.splanes.com.core.domain.Book
 
 class GetBookUseCase(private val bookRepository: BookRepository) {
-    suspend operator fun invoke(bookId: Int) : Book = bookRepository.getBook(bookId)
+    suspend operator fun invoke(
+        
+        bookId: Int,
+        onSuccess: (books: Book) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+    
+    ) = bookRepository.getBook(bookId = bookId, onSuccess = onSuccess, onError = onError)
 }
