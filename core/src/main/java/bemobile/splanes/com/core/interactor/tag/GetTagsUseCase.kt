22c803210ee261150796d953062375e8832bb920
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:21:26 +0100

    Update GetTagsUseCase.kt

--- core/src/main/java/bemobile/splanes/com/core/interactor/tag/GetTagsUseCase.kt
+++ core/src/main/java/bemobile/splanes/com/core/interactor/tag/GetTagsUseCase.kt
@@ -2,7 +2,14 @@ package bemobile.splanes.com.core.interactor.tag
 
 import bemobile.splanes.com.core.data.repository.TagRepository
 import bemobile.splanes.com.core.domain.Book
+import bemobile.splanes.com.core.domain.Tag
 
 class GetTagsUseCase(private val tagRepository: TagRepository) {
-    suspend operator fun invoke(book: Book) = tagRepository.getTags(book)
+    suspend operator fun invoke(
+
+        book: Book,
+        onSuccess: (tags: List<Tag>) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+    
+    ) = tagRepository.getTags(book = book, onSuccess = onSuccess, onError = onError)
 }
