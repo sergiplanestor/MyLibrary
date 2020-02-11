6762f049bfb0e5beb672a1e4e34e06d0dfefd560
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 13:25:48 +0100

    Update SharedPreferencesHelper.kt

--- app/src/main/java/bemobile/splanes/com/mylibrary/framework/helper/SharedPreferencesHelper.kt
+++ app/src/main/java/bemobile/splanes/com/mylibrary/framework/helper/SharedPreferencesHelper.kt
@@ -1,6 +1,40 @@
 package bemobile.splanes.com.mylibrary.framework.helper
 
-object SharedPreferencesHelper {
+import android.content.Context
+import android.content.SharedPreferences
 
+class SharedPreferencesHelper(context: Context) {
+
+// =================================================================================================
+// Keys
+// =================================================================================================
+
+    companion object Key {
+        private const val SHARED_NAME: String = "my_library.shared.prefs"
+        const val PREF_USR: String = "my_library.shared.prefs.usr"
+        const val PREF_PWD: String = "my_library.shared.prefs.pwd"
+    }
+
+// =================================================================================================
+// Attributes
+// =================================================================================================
+
+    private val mSharedPrefs: SharedPreferences
+
+// =================================================================================================
+// Init block
+// =================================================================================================
+
+    init {
+        mSharedPrefs = context.getSharedPreferences(SHARED_NAME, Context.MODE_PRIVATE)
+    }
+
+// =================================================================================================
+// Helper methods
+// =================================================================================================
+
+    fun putString(key: String, value: String) = mSharedPrefs.edit().run { putString(key, value) }.apply()
+
+    fun getString(key: String) : String? = mSharedPrefs.getString(key, null)
 
 }
