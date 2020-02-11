8867357ff51e7ae0adfad2280ad9fa3d074a5e4d
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:05:42 +0100

    Update TagDataSource.kt

--- core/src/main/java/bemobile/splanes/com/core/data/datasource/TagDataSource.kt
+++ core/src/main/java/bemobile/splanes/com/core/data/datasource/TagDataSource.kt
@@ -5,9 +5,23 @@ import bemobile.splanes.com.core.domain.Tag
 
 interface TagDataSource {
 
-    suspend fun fetch(book: Book) : List<Tag>
+    suspend fun fetch(
+        book: Book,
+        onRequestSuccess: (tags: List<Tag>) -> Unit,
+        onRequestError: (throwable: Throwable) -> Unit
+    )
 
-    suspend fun add(tags: List<Tag>, book: Book)
+    suspend fun add(
+        tags: List<Tag>,
+        book: Book,
+        onRequestSuccess: (isTagAdded: Boolean) -> Unit,
+        onRequestError: (throwable: Throwable) -> Unit
+    )
 
-    suspend fun remove(tag: Tag, book: Book)
+    suspend fun remove(
+        tag: Tag,
+        book: Book,
+        onRequestSuccess: (isTagRemoved: Boolean) -> Unit,
+        onRequestError: (throwable: Throwable) -> Unit
+    )
 }
