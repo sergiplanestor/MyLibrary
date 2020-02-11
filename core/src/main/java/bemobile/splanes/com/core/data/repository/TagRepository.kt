91bba25a6eebc01e4881b15bc7c31067a2f7301e
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:07:03 +0100

    Update TagRepository.kt

--- core/src/main/java/bemobile/splanes/com/core/data/repository/TagRepository.kt
+++ core/src/main/java/bemobile/splanes/com/core/data/repository/TagRepository.kt
@@ -6,9 +6,29 @@ import bemobile.splanes.com.core.domain.Tag
 
 class TagRepository(private val tagDataSource: TagDataSource) {
 
-    suspend fun addTag(tags: List<Tag>, book: Book) = tagDataSource.add(tags, book)
+    suspend fun addTag(
 
-    suspend fun getTags(book: Book) : List<Tag> = tagDataSource.fetch(book)
+        tags: List<Tag>,
+        book: Book,
+        onSuccess: (isTagAdded: Boolean) -> Unit,
+        onError: (throwable: Throwable) -> Unit
 
-    suspend fun removeTag(tag: Tag, book: Book) = tagDataSource.remove(tag, book)
+    ) = tagDataSource.add(tags = tags, book = book, onRequestSuccess = onSuccess, onRequestError = onError)
+
+    suspend fun getTags(
+
+        book: Book,
+        onSuccess: (tags: List<Tag>) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+
+    ) = tagDataSource.fetch(book = book, onRequestSuccess = onSuccess, onRequestError = onError)
+
+    suspend fun removeTag(
+
+        tag: Tag,
+        book: Book,
+        onSuccess: (isTagRemoved: Boolean) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+
+    ) = tagDataSource.remove(tag = tag, book = book, onRequestSuccess = onSuccess, onRequestError = onError)
 }
