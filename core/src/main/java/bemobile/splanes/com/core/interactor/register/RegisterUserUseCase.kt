785fc6d85efed9d278e8061c939938cbee5caa1d
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:18:37 +0100

    Update RegisterUserUseCase.kt

--- core/src/main/java/bemobile/splanes/com/core/interactor/register/RegisterUserUseCase.kt
+++ core/src/main/java/bemobile/splanes/com/core/interactor/register/RegisterUserUseCase.kt
@@ -4,5 +4,11 @@ import bemobile.splanes.com.core.data.repository.UserRepository
 import bemobile.splanes.com.core.domain.User
 
 class RegisterUserUseCase(private val userRepository: UserRepository) {
-    suspend operator fun invoke(user: User) = userRepository.registerUser(user)
+    suspend operator fun invoke(
+        
+        user: User,
+        onSuccess: (isUserRegistered: Boolean) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+    
+    ) = userRepository.registerUser(user = user, onSuccess = onSuccess, onError = onError)
 }
