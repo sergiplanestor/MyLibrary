3466f138402a0f8246ddd28e8267fe66f8ff2c68
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 11:05:20 +0100

    Update BookDataSource.kt

--- core/src/main/java/bemobile/splanes/com/core/data/datasource/BookDataSource.kt
+++ core/src/main/java/bemobile/splanes/com/core/data/datasource/BookDataSource.kt
@@ -4,13 +4,32 @@ import bemobile.splanes.com.core.domain.Book
 
 interface BookDataSource {
 
-    suspend fun add(book: Book, callback: (success: Boolean) -> Unit)
+    suspend fun add(
+        book: Book,
+        onRequestSuccess: (isBookAdded: Boolean) -> Unit,
+        onRequestError: (throwable: Throwable) -> Unit
+    )
 
-    suspend fun fetchAll(callback: (books: List<Book>) -> Unit)
+    suspend fun fetchAll(
+        onRequestSuccess: (books: List<Book>) -> Unit,
+        onRequestError: (throwable: Throwable) -> Unit
+    )
 
-    suspend fun fetch(id: Int, callback: (book: Book) -> Unit)
+    suspend fun fetch(
+        id: Int,
+        onRequestSuccess: (book: Book) -> Unit,
+        onRequestError: (throwable: Throwable) -> Unit
+    )
 
-    suspend fun remove(book: Book, callback: (success: Boolean) -> Unit)
+    suspend fun remove(
+        book: Book,
+        onRequestSuccess: (isBookRemoved: Boolean) -> Unit,
+        onRequestError: (throwable: Throwable) -> Unit
+    )
 
-    suspend fun update(book: Book, callback: (success: Boolean) -> Unit)
+    suspend fun update(
+        book: Book,
+        onRequestSuccess: (isBookUpdated: Boolean) -> Unit,
+        onRequestError: (throwable: Throwable) -> Unit
+    )
 }
