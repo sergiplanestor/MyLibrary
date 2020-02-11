208ce94d0416d441512a28267327cc61e9d5c480
splanes <59670429+sergiplanestor@users.noreply.github.com>
2020-02-11 13:37:29 +0100

    Update UserService.kt

--- app/src/main/java/bemobile/splanes/com/mylibrary/framework/user/UserService.kt
+++ app/src/main/java/bemobile/splanes/com/mylibrary/framework/user/UserService.kt
@@ -2,15 +2,49 @@ package bemobile.splanes.com.mylibrary.framework.user
 
 import bemobile.splanes.com.core.data.datasource.UserDataSource
 import bemobile.splanes.com.core.domain.User
+import bemobile.splanes.com.mylibrary.framework.helper.SharedPreferencesHelper
 import bemobile.splanes.com.mylibrary.framework.rest.RestApiDataSource
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-class UserService(private val restApiDataSource: RestApiDataSource) : UserDataSource {
-
+class UserService(
+    private val restApiDataSource: RestApiDataSource,
+    private val sharedPreferencesHelper: SharedPreferencesHelper
+) : UserDataSource {
+    
     override fun getStoredUser(): User? {
-        return null
+
+        val name = sharedPreferencesHelper.getString(SharedPreferencesHelper.PREF_USR)
+        val pwd = sharedPreferencesHelper.getString(SharedPreferencesHelper.PREF_PWD)
+
+        return  if (name != null && pwd != null) {
+            User(name, pwd)
+        } else {
+            null
+        }
     }
 
-    override suspend fun registerUser(user: User) : Boolean {
-        return restApiDataSource.register(user).blockingGet()
+    override fun storeUser(user: User) {
+        sharedPreferencesHelper.putString(SharedPreferencesHelper.PREF_USR, user.name)
+        sharedPreferencesHelper.putString(SharedPreferencesHelper.PREF_PWD, user.pwd)
+    }
+
+    override suspend fun registerUser(
+        user: User,
+        onRequestSuccess: (isUserRegistered: Boolean) -> Unit,
+        onRequestError: (throwable: Throwable) -> Unit
+    ) {
+        val disposable = restApiDataSource.register(user = user)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { success: Boolean ->
+                    if (success) {
+                        storeUser(user)
+                    }
+                    onRequestSuccess(success)
+                },
+                onRequestError
+            )
     }
 }
